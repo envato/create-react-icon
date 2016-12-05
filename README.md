b8056b7b7e78eda2abdb3a3c96ec3b0cde83cd4b
Fraser Xu <xvfeng123@gmail.com>
2016-12-05 11:38:24 +1100

    init project

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # create-react-icon
 Convert one or multiple svg files into React component
+
+## Installation
+
+```sh
+$ npm install create-react-icon -g
+```
+
+## Usage
+
+```sh
+$ create-react-icon ./alert.svg dist
+$ create-react-icon ./*.svg dist
+```
+
+## Result
+
+Your source svg file:
+
+```xml
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M200.4 54.1c-1.9-18.7 8.4-32.9 25.8-32.9h63.1c17.4 0 27.7 14.2 25.8 32.9l-27.7 242.2c-1.9 17.4-13.5 28.3-29.6 28.3s-27.7-11-29.6-28.3L200.4 54.1zm57.3 313.1c32.2 0 56 23.8 56 54.8v1.3c0 30.9-23.8 54.8-56 54.8s-56-23.8-56-54.8V422c0-31 23.8-54.8 56-54.8z"/></svg>
+
+```
+
+Becomes to :
+
+```js
+import React from 'react'
+import Icon from 'react-icon-base'
+
+export default (props) => {
+  return (
+    <Icon viewBox='0 0 512 512' {...props}>
+      <title id='alert'>{props.title || 'alert'}</title>
+      <g><path d="M200.4 54.1c-1.9-18.7 8.4-32.9 25.8-32.9h63.1c17.4 0 27.7 14.2 25.8 32.9l-27.7 242.2c-1.9 17.4-13.5 28.3-29.6 28.3s-27.7-11-29.6-28.3L200.4 54.1zm57.3 313.1c32.2 0 56 23.8 56 54.8v1.3c0 30.9-23.8 54.8-56 54.8s-56-23.8-56-54.8V422c0-31 23.8-54.8 56-54.8z"/></g>
+    </Icon>
+  )
+}
+```
+
+## How to use icons
+
+```js
+import AlertIcon from 'alertIcon'
+
+export default (props) => {
+   return (<div>
+     <AlertIcon title='alert icon' /> Manage my Collections
+  </div>)
+}
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const cheerio = require('cheerio')
+const camelcase = require('camelcase')
+const path = require('path')
+const mkdirp = require('mkdirp')
+const forEach = require('lodash.foreach')
+const titleize = require('titleize')
+const minimist = require('minimist')
+
+const argv = minimist(process.argv.slice(2))
+const src = argv._.slice(0, argv._.length - 1)
+const distDir = argv._.pop()
+
+if (!src || !distDir) {
+  throw new Error('Please check you argument.')
+}
+
+const cleanAtrributes = ($el, $) => {
+  const attrs = ['xlink:href', 'clip-path', 'fill-opacity', 'fill']
+
+  forEach(attrs, (attr) => {
+    $el.removeAttr(attr)
+  })
+  if ($el.children().length === 0) {
+    return false
+  }
+
+  $el.children().each((index, el) => {
+    cleanAtrributes($(el), $)
+  })
+}
+
+const createComponentString = (viewBox, iconSvg, name) => {
+  return `import React from 'react'
+import Icon from 'react-icon-base'
+
+export default (props) => {
+  return (
+    <Icon viewBox='${viewBox}' {...props}>
+      <title id='${titleize(name)}'>{props.title || '${titleize(name)}'}</title>
+      <g>${iconSvg}</g>
+    </Icon>
+  )
+}
+`
+}
+
+mkdirp(distDir, (err) => {
+  if (err) throw new Error('Error creating icons directory.')
+
+  src.map((iconPath) => {
+    const id = path.basename(iconPath, '.svg')
+    const svg = fs.readFileSync(iconPath, 'utf-8')
+    let $ = cheerio.load(svg, {
+      xmlMode: true
+    })
+    const $svg = $('svg')
+    cleanAtrributes($svg, $)
+    const iconSvg = $svg.html()
+    const viewBox = $svg.attr('viewBox')
+    const componentString = createComponentString(viewBox, iconSvg, id)
+
+    fs.writeFileSync(path.join(distDir, camelcase(id) + '.js'), componentString, 'utf-8')
+    return id
+  })
+})
